9969942d51fc42577c6107fc51092c8b9c0f6070
AlanPoAlsk <alsk91@gmail.com>
2024-05-02 19:27:12 -0400

    Deploy project to GitHub Pages fixed5

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+   // Resolve configuration
+  resolve: {
+    // Ensure that .jsx files are treated as JavaScript files
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  build: {
+    outDir: 'dist', 
+    assetsDir: 'assets', 
+    base: '/Portfolio/',
+  },
+});
